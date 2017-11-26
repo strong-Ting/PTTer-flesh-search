b53543e5c8c19b426adbd72f2c7486564d1b56e1
strongTing <s8527502987@gmail.com>
2017-11-26 14:19:46 +0800

    2017/11/26 14:19 Added: -when the post can't crawl,the error message is displayed
    Improved:
    -the info of the post display
    -when the post doesn't have the comments of the searched ID will dipsaly
    message
    
    Todo:
    -connect to PTT that websocket version,when clicked ID will start to
    searching
    -rearrange code
    -improve the display

--- js/content.js
+++ js/content.js
@@ -13,12 +13,38 @@ const hide_result = ()=>{
 }
 
 const display = (post_info)=>{
-	let result= document.getElementsByClassName('gsc-webResult gsc-result');
+
+	let result= document.querySelector('.gsc-results.gsc-webResult').querySelectorAll('.gs-webResult.gs-result');  
 	let post_num = post_info.num;
 
-	result[post_num].querySelector('a.gs-title').innerText = post_info.tittle;
-	result[post_num].querySelector('a.gs-title').href = post_info.href;
-	result[post_num].querySelector('.gs-bidi-start-align.gs-visibleUrl.gs-visibleUrl-long').innerText = "作者："+post_info.author+"    "+"IP："+post_info.ip+"時間:"+post_info.time;
+	if(post_info.author && post_info.href && post_info.ip &&post_info.title &&post_info.time){
+
+		let title = result[post_num].querySelector('a.gs-title');
+		title.innerText = post_info.title;
+		title.href = post_info.href;
+		title.target = '_blank'; //open link on new tab
+
+		let details_class = 'gs-bidi-start-align gs-visibleUrl gs-visibleUrl-long';
+		
+		let author = document.createElement('div');
+		author.className = details_class;
+		author.innerText = '作者：'+post_info.author;
+
+		let time = document.createElement('div');
+		time.className = details_class;
+		time.innerText = '時間：'+post_info.time;
+
+		let ip = document.createElement('div');
+		ip.className = details_class;
+		ip.innerText = 'IP:'+post_info.ip;
+
+		result[post_num].querySelector('.gsc-url-top').appendChild(author);
+		result[post_num].querySelector('.gsc-url-top').appendChild(time);
+		result[post_num].querySelector('.gsc-url-top').appendChild(ip);
+		
+
+//		result[post_num].querySelector('.gs-bidi-start-align.gs-visibleUrl.gs-visibleUrl-long').innerText = "作者："+post_info.author+"    "+"IP："+post_info.ip+"時間:"+post_info.time;
+	}
 
 	let content = result[post_num].querySelector('div.gs-snippet');
 	if(post_info.is_author==true){
@@ -66,33 +92,68 @@ const display = (post_info)=>{
 	else if(post_info.push.length>0){
 		content.innerText = "";
 		for(let i=0;i<post_info.push.length;i++){
-			content.innerText += (post_info.push[i].content+post_info.push[i].time);
+			content.innerText += (post_info.push[i].push_tag+post_info.keywords+post_info.push[i].content+post_info.push[i].time);
 		}
+	}
+	else{
+		content.innerText = "無符合搜尋ID的發言";
+	}
+	
+}
+
+const display_error= (error)=>{
+	
+	let href = error.responseURL;
+	
+	let result= document.querySelector('.gsc-results.gsc-webResult').querySelectorAll('.gs-webResult.gs-result');  
+
+
+	for(let i=0;i<result.length;i++){
+
+		let title =result[i].querySelector('a.gs-title');
+
+		if(href == title.href){
+
+			title.target = '_blank'; //open link on new tab
+			let wraning = document.createElement('wraning');
+			wraning.innerText = "錯誤訊息："+error.status;
+			wraning.style = 
+				` background-color: #f44336;
+				color: white;` ;
+			result[i].querySelector('.gsc-url-top').appendChild(wraning);
+		}	
 	}
 	
 }
 
 dom_listenser ('body',(mutations)=>{
     mutations.forEach(async(mutation)=>{
-	for(let i=0;i<mutation.addedNodes.length;i++){
-		if(mutation.addedNodes[i].className=='gsc-webResult gsc-result'){
-			//hide_result();
-			let keywords = document.getElementById('gs_tti50').lastChild.value;
-			try{
-				let result =mutation.addedNodes[i].querySelector('a.gs-title').href;
-				
-				let PTT = new crawl_PTT(result,keywords);
-				let post = await PTT.crawl();
-				let post_info = PTT.analysis(post);
-				console.log(post_info);
-				display(post_info);
-			}
-			catch(e){
-				console.log(e);
-				console.log(mutation);
+		for(let i=0;i<mutation.addedNodes.length;i++){
+			if(mutation.addedNodes[i].className=='gsc-webResult gsc-result'){
+				//hide_result();
+				let keywords,
+					result;
+				try{
+					keywords = document.getElementById('gs_tti50').lastChild.value;
+					result = mutation.addedNodes[i].querySelector('a.gs-title').href;
+				}
+				catch(e){
+					console.log(e);
+				}
+
+				try{
+					let PTT = new crawl_PTT(result,keywords);
+					let post = await PTT.crawl();
+					let post_info = PTT.analysis(post);
+					console.log(post_info);
+					display(post_info);
+				}
+				catch(e){
+					console.log(e);
+					display_error(e);
+				}
 			}
 		}
-	}
 	});
 });
 
@@ -150,13 +211,21 @@ class crawl_PTT{
 			board="",
 			author="",
 			is_author,
-			tittle="",
+			title="",
 			time="",
 			content="",
 			content_text="",
 			push=[],
 			ip="",
 			num="";
+
+		let custom_search = document.querySelector('.gsc-results.gsc-webResult').querySelectorAll('.gsc-thumbnail-inside');    
+		for(let i=0;i<custom_search.length;i++){
+			if(custom_search[i].querySelector('a.gs-title').href==this.href){
+				num = i;
+			}
+		}
+
 		try{
 			post_detail = post.getElementsByClassName('article-metaline');
 		}catch(e){
@@ -185,7 +254,7 @@ class crawl_PTT{
 		}
 
 		try{
-			tittle = post_detail[1].getElementsByClassName('article-meta-value')[0].innerText;
+			title = post_detail[1].getElementsByClassName('article-meta-value')[0].innerText;
 		}catch(e){
 
 			console.log(e);
@@ -217,20 +286,21 @@ class crawl_PTT{
 			let push_sel = content.getElementsByClassName('push');
 			for(let i=0;i<push_sel.length;i++){
 				let push_id = push_sel[i].getElementsByClassName('f3 hl push-userid')[0].innerText;
-				if(push_id==this.keywords)
+				if(push_id.replace(/\s+/g,"") ==this.keywords)
 				{
 					let push_content = push_sel[i].getElementsByClassName('f3 push-content')[0].innerText;
 					let push_time = push_sel[i].getElementsByClassName('push-ipdatetime')[0].innerText;
-/*
+					let push_tag = push_sel[i].getElementsByClassName('push-tag')[0].innerText;
+					/*
 					console.log('///////////////////////////////');
 					console.log(push_content+push_time);
 					console.log('///////////////////////////////');
 */
-					push.push({'push_num':i,'content':push_content,'time':push_time});
+					push.push({'push_num':i,'push_tag':push_tag,'content':push_content,'time':push_time});
 				}
 			}
 		}catch(e){
-			console.log(e);
+			console.log(num +':'+ e);
 		}
 
 		try{
@@ -250,30 +320,26 @@ class crawl_PTT{
 			console.log(e);
 		}
 
-		let custom_search =document.getElementsByClassName('gsc-webResult gsc-result');
-		for(let i=0;i<custom_search.length;i++){
-			if(custom_search[i].querySelector('a.gs-title').href==this.href){
-				num = i;
-			}
-		}
+
 		
 		this.post_info ={
 			'num':num,
 			'author':author,
 			'is_author':is_author,
 			'board':board,
-			'tittle':tittle,
+			'title':title,
 			'time':time,
 			'href':this.href,
 			'content_text':content_text,
 			'push':push,
-			'ip':ip
+			'ip':ip,
+			'keywords':this.keywords
 		};
 		
 /*
 		console.log('作者：'+author);
 		console.log('看板：'+board);
-		console.log('標題：'+tittle);
+		console.log('標題：'+title);
 		console.log('時間：'+time);
 */
 //		console.log(this.post_info);
