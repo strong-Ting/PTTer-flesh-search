9810c7ee3f1cfe98a3a87b7f8796bc99b353a6dc
strongTing <s8527502987@gmail.com>
2017-11-28 23:53:39 +0800

    2017/11/28 23:53 Added -crawl the container that push ID

--- js/ptt.js
+++ js/ptt.js
@@ -1,1 +1,19 @@
-console.log('test');
+
+
+const dom_listenser=(idName,mutationHandler)=>{
+        let target = document.querySelector(idName);
+        let config = { attributes: true, childList: true, characterData: true,subtree:true};
+        let observer = new MutationObserver(mutationHandler);
+        observer.observe(target, config);
+};
+
+dom_listenser('body',(mutations)=>{
+	mutations.forEach((mutation)=>{
+		if(mutation.target.tagName == 'SPAN'){
+			console.log(mutation.addedNodes.length);
+			for(let i=0;i<mutation.addedNodes.length;i++){
+				console.log(mutation.addedNodes[i].querySelector('.q11.b0'));
+			}
+		}
+	});
+});
