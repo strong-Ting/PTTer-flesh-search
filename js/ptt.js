887f75908eb5e8cf550a0c28fd31a4850c247ee5
strongTing <s8527502987@gmail.com>
2017-11-29 23:35:32 +0800

    2017/11/29 23:35 Added -add css,but now useless -can click ID of push to open new window

--- js/ptt.js
+++ js/ptt.js
@@ -1,3 +1,15 @@
+//add css
+(()=>{
+	let add_style = document.createElement('style');
+	add_style.type = 'text/css';
+	add_style.innerHTML =`
+.push {
+    color: #ff0;
+	background-color: transparent;
+}
+`
+	document.head.appendChild(add_style);
+})();
 
 
 const dom_listenser=(idName,mutationHandler)=>{
@@ -7,12 +19,24 @@ const dom_listenser=(idName,mutationHandler)=>{
         observer.observe(target, config);
 };
 
+
 dom_listenser('body',(mutations)=>{
 	mutations.forEach((mutation)=>{
-		if(mutation.target.tagName == 'SPAN'){
-			console.log(mutation.addedNodes.length);
-			for(let i=0;i<mutation.addedNodes.length;i++){
-				console.log(mutation.addedNodes[i].querySelector('.q11.b0'));
+		let target =mutation.target;
+		let added = mutation.addedNodes;
+		if(target.tagName == 'SPAN'){
+			try{
+				for(let i=0;i<added.length;i++){
+					let push = added[i].querySelector('.q11.b0');
+					if(push){
+						push.className = "push";
+						push.setAttribute('role','button');
+						push.onclick = ()=>{window.open('https://www.google.com');}
+					}
+				}
+			}
+			catch(e){
+				console.log(added);
 			}
 		}
 	});
