80caa67f8ed6ed46eb8520950760d17d747cfbbb
strongTing <s8527502987@gmail.com>
2017-12-05 20:03:28 +0800

    fix the problem that the search window not work sometimes

--- js/search.js
+++ js/search.js
@@ -1,12 +1,25 @@
 
 const search = (ID)=>{
-	document.getElementById('gs_tti50').lastChild.value =  ID;
-	document.getElementsByClassName('gsc-search-button gsc-search-button-v2')[0].click();
+	try{
+		document.getElementById('gs_tti50').lastChild.value =  ID;
+		document.getElementsByClassName('gsc-search-button gsc-search-button-v2')[0].click();
+	}
+	catch(e){
+		console.log(ID);
+	//	alert(ID);
+		setTimeout("search('"+ID+"');",50);
+	}
 }
 
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {  
 	console.log(message);	
-	search(message.ID);
-	sendResponse('search');  
+	try{
+		search(message.ID);
+		sendResponse('search');  
+	}
+	catch(e){
+		alert('oops have someting wrong');
+		console.log(e);
+	}
 });
